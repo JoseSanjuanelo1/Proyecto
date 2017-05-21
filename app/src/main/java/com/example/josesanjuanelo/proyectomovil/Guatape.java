c1a6a5a66db3546671940666ce22435746febfe3
Jose Sanjuanelo <j-os-pablo@hotmail.com>
2017-05-21 18:07:52 -0500

    Avance v1.4

--- app/src/main/java/com/example/josesanjuanelo/proyectomovil/Guatape.java
+++ app/src/main/java/com/example/josesanjuanelo/proyectomovil/Guatape.java
@@ -1,7 +1,9 @@
 package com.example.josesanjuanelo.proyectomovil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Guatape extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Guatape extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guatape);
+    }
+
+    public void mostrar_mapa (View v){
+        Intent i = new Intent(Guatape.this,Mapa_Guatape.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/josesanjuanelo/proyectomovil/Lejania.java
+++ app/src/main/java/com/example/josesanjuanelo/proyectomovil/Lejania.java
@@ -1,7 +1,7 @@
 package com.example.josesanjuanelo.proyectomovil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Lejania extends AppCompatActivity {
 
